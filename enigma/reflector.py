78d2c8a1c682bee4288cbbd0b11f77a9ff30915b
Jonas Düsterwald <jonas.duesterwald@th-deg.de>
2023-03-22 23:52:49 +0100

    Added basic wirings for Enigma 1, B, M3

---
+++ enigma/reflector.py
@@ -0,0 +1,29 @@
+
+class Reflector:
+
+    def __int__(self):
+        self.wiring = ""
+
+
+    def set_wiring_B(self, choice):
+        # TODO:
+        pass
+        #if choice == 1:
+        #    self.wiring =
+
+
+    def set_wiring_ONE(self, choice):
+        if choice == 1:
+            self.wiring = "EJMZALYXVBWFCRQUONTSPIKHGD"
+        elif choice == 2:
+            self.wiring = "YRUHQSLDPXNGOKMIEBFZCWVJAT"
+        else:
+            self.wiring = "FVPJIAOYEDRZXWGCTKUQSBNMHL"
+
+
+    def set_wiring_M3(self, choice):
+
+        if choice == 1:
+            self.wiring = "YRUHQSLDPXNGOKMIEBFZCWVJAT"
+        else:
+            self.wiring = "FVPJIAOYEDRZXWGCTKUQSBNMHL"
