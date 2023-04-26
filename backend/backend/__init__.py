cbcab9e5b145c1fb70323748a040fb47f127dd01
ZSchoen <dev@zschoen.dev>
2023-04-26 17:41:10 +0200

    basic cors origin in backend

--- backend/backend/__init__.py
+++ backend/backend/__init__.py
@@ -8,6 +8,11 @@ def create_app(test_config=None):
     if test_config is not None:
         app.config.from_mapping(test_config)
 
+    @app.after_request
+    def add_cors(response):
+        response.headers.set('Access-Control-Allow-Origin', '*')
+        return response
+
     with app.app_context():
         from . import views
 
