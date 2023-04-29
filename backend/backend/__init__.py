2790cb6c012dd0489a2d87336788344d060d812d
ZSchoen <dev@zschoen.dev>
2023-04-29 16:25:07 +0200

    rm CORS from backend

--- backend/backend/__init__.py
+++ backend/backend/__init__.py
@@ -8,14 +8,6 @@ def create_app(test_config=None):
     if test_config is not None:
         app.config.from_mapping(test_config)
 
-    @app.after_request
-    def add_cors(response):
-        response.headers.set('Access-Control-Allow-Origin', '*')
-        response.headers.set('Access-Control-Allow-Methods', 'GET, PUT')
-        response.headers.set('Access-Control-Expose-Headers', '*')
-        response.headers.set('Access-Control-Allow-Credentials', 'true')
-        return response
-
     with app.app_context():
         from . import views
 
