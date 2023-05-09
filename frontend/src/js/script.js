f8c77a23642685b5f4ff0408e108552c9612dda7
Woldemar Loginow <woldemar.loginow@stud.th-deg.de>
2023-05-09 15:36:46 +0200

    Create keyboard keys without function

--- frontend/src/js/script.js
+++ frontend/src/js/script.js
@@ -3,6 +3,11 @@ const keyboard_input = document.getElementById('keyboard_input');
 const maxHistoryLength = 140;
 const backendUrl = 'http://localhost:5000';
 
+const rows = [
+  'qwertyuiop',
+  'asdfghjkl',
+  'zxcvbnm'
+];
 
 // Senden und Empfangen von Daten vom Backend
 async function putToBackend(directory, data) {
@@ -52,3 +57,25 @@ function getVariants() {
 function putVariant(data) {
   return putToBackend('/variant', data);
 }
+
+// Erstellen der Tasten
+function createKeys(keyboardDiv) {
+  for (let rowIndex = 0; rowIndex < rows.length; rowIndex++) {
+    const row = document.createElement('div');
+    row.classList.add('row');
+    const keys = rows[rowIndex];
+
+    for (let i = 0; i < keys.length; i++) {
+      const key = document.createElement('div');
+      key.classList.add('key_input');
+      key.textContent = keys[i].toUpperCase();
+
+      row.appendChild(key);
+    }
+
+    keyboardDiv.appendChild(row);
+  }
+}
+
+createKeys(keyboard_input);
+
