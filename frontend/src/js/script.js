9b13d3ba080657ab80ba26eea1a2168136ffbacd
Woldemar Loginow <woldemar.loginow@stud.th-deg.de>
2023-05-13 10:21:45 +0200

    change backend url

--- frontend/src/js/script.js
+++ frontend/src/js/script.js
@@ -1,7 +1,7 @@
 const outputHistory = document.getElementById('outputHistory');
 const keyboard_input = document.getElementById('keyboard_input');
 const maxHistoryLength = 140;
-const backendUrl = 'http://localhost:5000';
+const backendUrl = 'http://localhost:8000/api';
 
 const rows = [
   'qwertyuiop',
