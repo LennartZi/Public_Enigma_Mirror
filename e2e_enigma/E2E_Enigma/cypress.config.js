760e0029895da8fc8bf1ed7b40455142da086287
ZSchoen <dev@zschoen.dev>
2023-05-17 17:39:15 +0200

    configure url via env

--- e2e_enigma/E2E_Enigma/cypress.config.js
+++ e2e_enigma/E2E_Enigma/cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   env: {
     MAIN_SERVICE: 'localhost:8000',
-  }
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
