b5838813f610476c222f558f2a42216dad707f37
Simon Ertl <simon.ertl2@stud.th-deg.de>
2023-06-04 15:06:15 +0200

    Für neues Frontend mit Rotor Selection

--- e2e_enigma/E2E_Enigma/cypress/e2e/spec.cy.js
+++ e2e_enigma/E2E_Enigma/cypress/e2e/spec.cy.js
@@ -2,6 +2,8 @@
 let output_history = "#outputHistory"
 let input_history = "#inputHistory"
 let keys = ".key_input"
+let model_selector = "#variantSelect"
+let model = "I"
 //Encrypt - Decrypt Test
 let usr_input = "HELLO"
 let encr_output = ""
@@ -10,22 +12,6 @@ let main_service = Cypress.env('MAIN_SERVICE')
 let url = 'http://' + main_service
 let api_encrypt = url + '/api/encrypt'
 
-// Custom Command To Test Input And Backend Respond
-Cypress.Commands.add('testUserInput', (inp) => {
-  for (let i = 0; i < inp.length; i++) {
-    cy.get(keys).contains(inp[i]).click()
-    cy.get(keys).contains(inp[i]).should('have.class', 'highlight')
-    cy.wait(65)
-  }
-
-  for (let i = 0; i < inp.length; i++) {
-    cy.get(keys).contains(inp[i]).not('have.class', 'highlight')
-  }
-
-  cy.get(output_history).invoke('text').then((text) => {
-    expect(text).to.have.length(inp.length)
-});
-})
 
 // Test Frontpage Loading
 describe('Visit Frontpage', () => {
@@ -47,28 +33,10 @@ describe('Test PUT', () => {
   })
 })
 
-// Test Single Click
-describe('Test Click', () => {
-  it('Single User Interation', () => {
-    cy.visit(url)
-    //  clicks Q, tests if highlighted, then if highlight gone, then if output field has 1 letter
-    cy.get(keys).contains('Q').click()
-    cy.get(keys).contains('Q').should('have.class', 'highlight')
-    cy.get(keys).contains('Q').not('have.class', 'highlight')
-    
-    cy.wait(65)
-    cy.get(output_history).invoke('text').then((text) => {
-      expect(text).to.have.length(1)
-  });
-
-
-  })
-})
-
 // Test Keyboard Input
 describe('Test Keyboard Input', () => {
   it('Single User Interation', () => {
-    cy.visit(url)
+    cy.setupTest(url, model_selector, model)
     cy.get('body').type('{q}');
     cy.wait(65)
     cy.get(output_history).invoke('text').then((text) => {
@@ -80,33 +48,37 @@ describe('Test Keyboard Input', () => {
 // Same as User Input but with every Letter
 describe('Test Every Letter', () => {
   it('Click Every Letter for Function Test', () => {
-    cy.visit(url)
+    cy.setupTest(url, model_selector, model)
+
     let inp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    cy.testUserInput(inp)
+    cy.testUserInput(input_history, output_history, keys, inp)
+    cy.checkHistory(input_history, output_history, inp)
   })
 })
 
+//Encryption
+describe('Full User Interaction', () => {
+  it('Full User Interaction', () => {
+    cy.setupTest(url, model_selector, model)
 
-// Normal User Interaction
-describe('User Interaction Encryption', () => {
-  it('User Types the Word '+usr_input, () => {
-    cy.visit(url)
-    cy.testUserInput(usr_input)
+    cy.testUserInput(input_history,output_history, keys, usr_input)
+    cy.checkHistory(input_history, output_history, usr_input)
 
     cy.get(output_history).invoke('text').then((text) => {
       encr_output = text
     });
+
   })
 })
 
 // Decryption
 describe('User Interaction Decryption', () => {
   it('User Types the Encrypted '+usr_input, () => {
-    cy.visit(url)
+    cy.setupTest(url, model_selector, model)
     //Da ein neuer Buchstabe immer links in die History und nicht rechts vom alten geschrieben wird,
     //muss Encrypteter String reversed werden fuer den Input
     encr_output = encr_output.split('').reverse().join("")
-    cy.testUserInput(encr_output)
+    cy.testUserInput(input_history,output_history, keys, encr_output)
 
     cy.get(output_history).invoke('text').then((text) => {
     expect(text).to.equal(usr_input.split('').reverse().join(""))
