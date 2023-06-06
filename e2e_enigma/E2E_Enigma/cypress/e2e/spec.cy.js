e0fda3cfccb91d6a73234d9310836a386a33dcf5
Moritz Schön <moritz.schoen@stud.th-deg.de>
2023-06-06 10:09:12 +0200

    Revert "test bigger wait"
    This reverts commit e35b6347f04544e3123725ff77499961feacb8fb

--- e2e_enigma/E2E_Enigma/cypress/e2e/spec.cy.js
+++ e2e_enigma/E2E_Enigma/cypress/e2e/spec.cy.js
@@ -38,7 +38,7 @@ describe('Test Keyboard Input', () => {
   it('Single User Interation', () => {
     cy.setupTest(url, model_selector, model)
     cy.get('body').type('{q}');
-    cy.wait(150)
+    cy.wait(65)
     cy.checkHistory(input_history, output_history, 'Q')
   })
 })
